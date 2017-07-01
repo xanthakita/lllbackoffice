25a8379d281a7a3bd8e53db3a6c62c7abc20ee50
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-06-30 20:18:44 -0400

    update database

--- Classes/mydbi.php
+++ Classes/mydbi.php
@@ -2,7 +2,7 @@
 // mysql connection info  ($mysqlserver, $mysqlusername, $mysqlpassword
 // This is for connectivity to the mysql database server on the nocbox01
 
-$mysqlserver='localhost';
+$mysqlserver='162.243.25.103';
 $mysqlusername='xanthakita';
 $mysqlpassword='6619Y^0m3';
 $dbname = 'lllbackoffice';
