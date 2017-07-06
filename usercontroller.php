4773f5eb08bd6c813b32c76ee3c1891f2387438f
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-06 12:16:44 -0400

    added kint

--- usercontroller.php
+++ usercontroller.php
@@ -22,8 +22,7 @@ session_start();
 
 if (isset($_COOKIE['uid'])) {$adminuser = json_decode($_COOKIE['uid']);} else { $adminuser = 'not set';}
 
-// kint::enabled(false);
-kint::trace;
+
 
 // d($_POST);
 //controlls the login process
