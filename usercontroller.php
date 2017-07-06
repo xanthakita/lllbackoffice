006cc629282d8111406604e09ae64d172d47ea5c
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-06 13:58:30 -0400

    adding artist image

--- usercontroller.php
+++ usercontroller.php
@@ -97,6 +97,26 @@ switch ($_POST['act']) {
 		$firstname = $_POST['firstname'];
 		$lastname  = $_POST['lastname'];
 		$email     = $_POST['email'];
+		$picture   = $_POST['artistimage'];
+
+		// images
+
+		$target_dir = "images/artists/";
+		$target_file = $target_dir . basename($_FILES["artistimage"][$username]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		    if($check !== false) {
+		        echo "File is an image - " . $check["mime"] . ".";
+		        $uploadOk = 1;
+		    } else {
+		        echo "File is not an image.";
+		        $uploadOk = 0;
+		    }
+		}
+
 		// $userdept  = $_POST['dept'];
 		// $userpriv  = $_POST['user_priv'];
 		if (isset($_POST['linenum'])) {
