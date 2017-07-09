e7e2e0b5b13dd630c636c32012e93a01d005d22f
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-09 13:50:39 -0400

    working on look and feel

--- index.php
+++ index.php
@@ -64,8 +64,8 @@ Copywrite @2015
 
 				
 				<!-- Main -->
-					<div id="main" class="center-text">
-         <div class="btn-group center-text">
+					<div id="main" class="text-center">
+         <div class="btn-group">
             <a class="btn btn-xs btn-primary" href="logout.php">Log Off</a>
             <a class="btn btn-xs btn-primary" href="#">Add Client</a>
             <a class="btn btn-xs btn-primary" href="#">Find Client</a>
