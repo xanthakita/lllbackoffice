87d4d5f759135776fbde6da7b4294886114633c7
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-09 13:36:07 -0400

    working on look and feel

--- index.php
+++ index.php
@@ -41,13 +41,16 @@ Copywrite @2015
 			
 			</div>
  <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">Navbar</a>
+
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+			Welcome, <?php echo $username;?>:<br>
+			<div class="spotlight pull-right"> <image src="./images/artists/<?php echo $username;?>.jpg" width="85px"  style="transform: rotate(90deg);">
+			</image>
+			</div>
+          </li>
           <li class="nav-item active">
             <a class="nav-link" href="#">Logout <span class="sr-only">(current)</span></a>
           </li>
@@ -60,12 +63,7 @@ Copywrite @2015
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Run Reports</a>
           </li>
-          <li class="nav-item">
-			Welcome, <?php echo $username;?>:<br>
-			<div class="spotlight"> <image src="./images/artists/<?php echo $username;?>.jpg" width="85px"  style="transform: rotate(90deg);">
-			</image>
-			</div>
-          </li>
+
         </ul>
       </div>
     </nav>
