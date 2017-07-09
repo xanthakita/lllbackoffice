361866adca91b1baad7b056497cd3d2e2adbaa76
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-09 12:47:34 -0400

    changing css

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ Copywrite @2015
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
+		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		
@@ -59,7 +59,7 @@ Copywrite @2015
 						<!-- Introduction -->
 						<div class="col-3-sm"></div>
 						<div class="col-6-sm">
-							Welcome, <?php echo $username;?>: <image src="./images/artists/<?php echo $username;?>.jpg" width="15%" height="15%" style="transform: rotate(90deg);"/>
+							Welcome, <?php echo $username;?>: <image class="spotlight" src="./images/artists/<?php echo $username;?>.jpg" width="15%" height="15%" style="transform: rotate(90deg);"/>
 						</div>
 						<div class="well center-text">
 							<h3>Please choose an action below</h3>
