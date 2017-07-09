e8a86409f5b31a55c911660016f2796bc861d92f
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-07-09 17:51:08 -0400

    added favicon

--- Classes/include.php
+++ Classes/include.php
@@ -12,9 +12,10 @@
  Copywrite @2017
  */
  ?>
- 
+
 	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
 	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+	<link rel=icon href=favicon.png sizes="16x16" type="image/png">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
