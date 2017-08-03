f948b81fc4c9afcdb7f7db7dd27d9b9593aad0e9
Jonathan Wagner <jonathan.wagner@windstream.com>
2017-08-03 01:33:11 -0400

    updating add client funtion

--- client.class.php
+++ client.class.php
@@ -217,6 +217,8 @@ Class Client {
 			$time = $now->format('Y-m-d H:i:s');
 			$imagename=strtolower($uid).".".substr($picture,-3);
 			$sql="Replace into lllbackoffice.clients (last_name, first_name, first_visit, phone, email, city, state, birth_month, birth_day, refered, refered_by, picture, added_by, ts) values ('$lastname', '$firstname', '$firstVisit', '$phone', '$email', '$city', '$state', '$birthmonth', '$birthday', '$refered', '$refered_by', '$picture, '$thisuser', '$time');";
+			var_dump($sql);
+			die;
 			$adduser=$clients->querydb($sql);
 
 	}
